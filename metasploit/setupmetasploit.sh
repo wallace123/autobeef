dba4364433bd13802d8f7eea832bcf9320705a01
wallace123 <wallace123@github.com>
2020-11-16 10:25:42 -0500

    using metasploit 6.0.15

--- metasploit/setupmetasploit.sh
+++ metasploit/setupmetasploit.sh
@@ -1,5 +1,6 @@
 sudo apt-get update && sudo apt-get -y full-upgrade
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+sudo apt install -y vim curl net-tools
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/6.0.15/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 chmod 755 msfinstall
 ./msfinstall
 
