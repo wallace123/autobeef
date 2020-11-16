e5f2ba0c54b6de6eacb2b5dd3e6a6496b56e62e5
wallace123 <wallace123@github.com>
2020-11-16 09:48:19 -0500

    Forgot to edit path for setupbeef.sh

--- beef/setupbeef.sh
+++ beef/setupbeef.sh
@@ -6,6 +6,6 @@ sudo apt update
 apt list --upgradable
 sudo apt -y upgrade
 cd beef
-/home/ubuntu/beef/install
-sed -i 's/passwd: "beef"/passwd: "abc123"/g' /home/ubuntu/beef/config.yaml
-/home/ubuntu/beef/beef
+./install
+sed -i 's/passwd: "beef"/passwd: "abc123"/g' ./config.yaml
+./beef
